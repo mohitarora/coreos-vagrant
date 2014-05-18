cfb26f8a177627e10a0a2addf68f5280f99b344a
marora <mailtomohitarora@gmail.com>
2014-05-18 19:17:05 -0400

    Fixed Httpd Error

--- httpd/launch.sh
+++ httpd/launch.sh
@@ -7,11 +7,11 @@ export ETCD=$HOST_IP:4001
 
 echo "[httpd] booting container. ETCD: $ETCD"
 
-until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml; do
+until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml > /var/log/confd.log 2>&1; do
   echo "[httpd] waiting for confd to refresh httpd-proxy.conf"
   sleep 5
 done
 
 # Run confd in the background to watch the upstream servers
-confd -interval 120 -quiet -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml
+nohup confd -interval 120 -quiet -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml > /var/log/confd.log 2>&1 &
 echo "[httpd] confd is listening for changes on etcd..."
