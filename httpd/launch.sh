ce77614cc3fe65f0273ed17e5e328ec6dd1897c3
marora <mailtomohitarora@gmail.com>
2014-05-18 18:46:46 -0400

    Fixed HTTP Error

--- httpd/launch.sh
+++ httpd/launch.sh
@@ -6,16 +6,6 @@ export HOST_IP=${HOST_IP:-172.17.8.101}
 export ETCD=$HOST_IP:4001
 
 echo "[httpd] booting container. ETCD: $ETCD"
-
-until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml; do
-  echo "[httpd] waiting for confd to refresh httpd-proxy.conf"
-  sleep 5
-done
-
-# Start apche
-echo "[httpd] starting apache service..."
-apachectl start
-
 # Run confd in the background to watch the upstream servers
-nohup confd -interval 120 -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml &
+nohup confd -interval 120 -quiet -node $ETCD -config-file /etc/confd/conf.d/httpd-proxy.toml
 echo "[httpd] confd is listening for changes on etcd..."
